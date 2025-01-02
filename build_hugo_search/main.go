2dc892ffe8aa8342b08eb5d4eab937c3279d02b1
yinyajun-hj <yinyajun-hj@huafang.com>
2025-01-02 13:33:04 +0800

    update

--- build_hugo_search/main.go
+++ build_hugo_search/main.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Begin to Build Hugo Algolia Search Index")
+	fmt.Println("== Begin to Build Hugo Algolia Search Index ==")
 	root := flag.String("root_dir", ".", "Root directory of Hugo Blog")
 	post := flag.String("post_dir", "posts", "Post directory of posts")
+	fmt.Println("[root_dir]:", *root)
+	fmt.Println("[post_dir]:", *post)
 
 	flag.Parse()
 
